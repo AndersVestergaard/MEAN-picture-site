42e8220b7bd93a61ca98f3b20815940cf30dc24b
Vestergarden <vestergarden@gmail.com>
2017-04-10 00:04:59 +0200

    videos start playing at the same time

--- hoey-scripts/index.js
+++ hoey-scripts/index.js
@@ -1,11 +1,35 @@
 class Index {
     static main() {
-        console.log('Hello World');
-        var u = "Msg to beaware of reload bug - only dev";
-        for (var i = 0; i < 10; i++) {
-            console.log(u);
+        this.mediaPlayers = [{ id: "media-player1", element: null, dataLoaded: false },
+            { id: "media-player2", element: null, dataLoaded: false },
+            { id: "media-player4", element: null, dataLoaded: false }];
+        document.addEventListener("DOMContentLoaded", function () { Index.initVideos(); }, false);
+    }
+    static initVideos() {
+        for (var i = 0; i < this.mediaPlayers.length; i++) {
+            this.mediaPlayers[i].element = document.getElementById(this.mediaPlayers[i].id);
+            if (this.mediaPlayers[i].element != null) {
+                this.mediaPlayers[i].element.controls = false;
+                this.mediaPlayers[i].element.loop = true;
+                this.mediaPlayers[i].element.onloadeddata = () => Index.videoLoaded();
+            }
         }
-        return 0;
+    }
+    static videoLoaded() {
+        //Test if all video data is loaded if true play
+        if (Index.isAllVideosLoaded()) {
+            for (var i = 0; i < this.mediaPlayers.length; i++) {
+                this.mediaPlayers[i].element.play();
+            }
+        }
+    }
+    static isAllVideosLoaded() {
+        for (var i = 0; i < this.mediaPlayers.length; i++) {
+            if (this.mediaPlayers[i].element == null || !(this.mediaPlayers[i].element.readyState === 4)) {
+                return false;
+            }
+        }
+        return true;
     }
 }
 Index.main();
--- hoey-scripts/index.ts
+++ hoey-scripts/index.ts
@@ -1,14 +1,47 @@
-class Index{
 
-    public static main(): number {
-        console.log('Hello World');
-        var u = "Msg to beaware of reload bug - only dev";
-        for(var i = 0; i < 10; i++){
-            console.log(u);
-        }
+class Index {
 
-        return 0;
+    static mediaPlayers: { id: string, element: HTMLVideoElement, dataLoaded: boolean }[];
+    static main() {
+        this.mediaPlayers = [{ id: "media-player1", element: null, dataLoaded: false },
+        { id: "media-player2", element: null, dataLoaded: false },
+        { id: "media-player4", element: null, dataLoaded: false }];
+        document.addEventListener("DOMContentLoaded", function () { Index.initVideos(); }, false);
     }
+
+
+    static initVideos(): void {
+        for (var i = 0; i < this.mediaPlayers.length; i++) {
+            this.mediaPlayers[i].element = document.getElementById(this.mediaPlayers[i].id) as HTMLVideoElement;
+            if (this.mediaPlayers[i].element != null) {
+                this.mediaPlayers[i].element.controls = false;
+                this.mediaPlayers[i].element.loop = true;
+                this.mediaPlayers[i].element.onloadeddata =  () => Index.videoLoaded();
+            }
+
+        }
+    }
+
+    private static videoLoaded() {
+        //Test if all video data is loaded if true play
+        if (Index.isAllVideosLoaded()) {
+            for (var i = 0; i < this.mediaPlayers.length; i++) {
+                this.mediaPlayers[i].element.play();
+            }
+        }
+    }
+
+    private static isAllVideosLoaded() {
+        for (var i = 0; i < this.mediaPlayers.length; i++) {
+            if (this.mediaPlayers[i].element == null || !(this.mediaPlayers[i].element.readyState === 4)) {
+                return false;
+            }
+        }
+        return true;
+
+    }
+
+
 
 }
 Index.main();
